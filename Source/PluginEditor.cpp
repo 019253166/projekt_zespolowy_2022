d6ff1020ed3bb6bd9d194fae41a0fac2b1200cb9
Jan Kucharczyk <kucharjan2@gmail.com>
2022-05-11 00:02:27 +0200

    pokrętła ogólnego przeznaczenia
    funkcjonalne pokrętła bez opisów

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,11 +9,69 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+int windowWidth = 1000;
+
 Placeholder::Placeholder()
 {
     juce::Random r;
     customColour = juce::Colour(r.nextInt(255), r.nextInt(255), r.nextInt(255));
 }
+
+//==============================================================================
+
+GlobalControls::GlobalControls(juce::AudioProcessorValueTreeState& apvts)
+{
+    using namespace Parameters;
+    const auto& parameters = GetParameters();
+
+    auto makeAttachmentHelper = [&parameters, &apvts](auto& attachment, const auto& name, auto& slider)
+    {
+        makeAttachment(attachment, apvts, parameters, name, slider);
+    };
+
+    makeAttachmentHelper(inputGainSliderAttachment, Names::Input_Gain, inputGainSlider);
+    makeAttachmentHelper(outputGainSliderAttachment, Names::Output_Gain, outputGainSlider);
+    makeAttachmentHelper(lowCrossoverSliderAttachment, Names::Low_LowMid_Crossover_Freq, lowCrossoverSlider);
+    makeAttachmentHelper(midCrossoverSliderAttachment, Names::LowMid_HighMid_Crossover_Freq, midCrossoverSlider);
+    makeAttachmentHelper(highCrossoverSliderAttachment, Names::HighMid_High_Crossover_Freq, highCrossoverSlider);
+
+    addAndMakeVisible(inputGainSlider);
+    addAndMakeVisible(outputGainSlider);
+    addAndMakeVisible(lowCrossoverSlider);
+    addAndMakeVisible(midCrossoverSlider);
+    addAndMakeVisible(highCrossoverSlider);
+}
+
+void GlobalControls::paint(juce::Graphics& g)
+{
+    using namespace juce;
+    auto bounds = getLocalBounds();
+    g.setColour(Colours::darkgrey);
+    g.fillAll();
+
+    bounds.reduce(3, 3);
+    g.setColour(Colours::black);
+    g.fillRoundedRectangle(bounds.toFloat(), 3);
+}
+
+void GlobalControls::resized()
+{
+    auto bounds = getLocalBounds();
+    using namespace juce;
+
+    FlexBox flexBox;
+    flexBox.flexDirection = FlexBox::Direction::row;
+    flexBox.flexWrap = FlexBox::Wrap::noWrap;
+
+    flexBox.items.add(FlexItem(inputGainSlider).withFlex(1.f));
+    flexBox.items.add(FlexItem(lowCrossoverSlider).withFlex(1.f));
+    flexBox.items.add(FlexItem(midCrossoverSlider).withFlex(1.f));
+    flexBox.items.add(FlexItem(highCrossoverSlider).withFlex(1.f));
+    flexBox.items.add(FlexItem(outputGainSlider).withFlex(1.f));
+
+    flexBox.performLayout(bounds);
+}
+
 //==============================================================================
 Projekt_zespoowy_2022AudioProcessorEditor::Projekt_zespoowy_2022AudioProcessorEditor (Projekt_zespoowy_2022AudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
@@ -25,7 +83,7 @@ Projekt_zespoowy_2022AudioProcessorEditor::Projekt_zespoowy_2022AudioProcessorEd
     addAndMakeVisible(bandLowMidControls);
     addAndMakeVisible(bandHighMidControls);
     addAndMakeVisible(bandHighControls);
-    setSize (800, 500);
+    setSize (windowWidth, 600);
 }
 
 Projekt_zespoowy_2022AudioProcessorEditor::~Projekt_zespoowy_2022AudioProcessorEditor()
@@ -49,8 +107,8 @@ void Projekt_zespoowy_2022AudioProcessorEditor::resized()
     // subcomponents in your editor..
     auto bounds = getLocalBounds();
     globalControls.setBounds(bounds.removeFromTop(64));
-    bandLowControls.setBounds(bounds.removeFromLeft(200));
-    bandLowMidControls.setBounds(bounds.removeFromLeft(200));
-    bandHighMidControls.setBounds(bounds.removeFromLeft(200));
-    bandHighControls.setBounds(bounds.removeFromLeft(200));
+    bandLowControls.setBounds(bounds.removeFromLeft(windowWidth / 4));
+    bandLowMidControls.setBounds(bounds.removeFromLeft(windowWidth / 4));
+    bandHighMidControls.setBounds(bounds.removeFromLeft(windowWidth / 4));
+    bandHighControls.setBounds(bounds.removeFromLeft(windowWidth / 4));
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -21,6 +21,44 @@ struct Placeholder : juce::Component
     }
     juce::Colour customColour;
 };
+
+struct RotarySlider : juce::Slider
+{
+    RotarySlider() :
+        juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                     juce::Slider::TextEntryBoxPosition::NoTextBox){}
+};
+
+template<
+    typename Attachment,
+    typename APVTS,
+    typename Params,
+    typename ParamName,
+    typename SliderType>
+
+void makeAttachment(std::unique_ptr<Attachment>& attachment, APVTS& apvts, const Params& parameters, const ParamName& name, SliderType& slider)
+{
+    attachment = std::make_unique<Attachment>(apvts, parameters.at(name), slider);
+}
+
+struct GlobalControls : juce::Component
+{
+    GlobalControls(juce::AudioProcessorValueTreeState& apvts);
+
+    void paint(juce::Graphics& g) override;
+
+    void resized() override;
+private:
+    RotarySlider inputGainSlider, outputGainSlider, lowCrossoverSlider, midCrossoverSlider, highCrossoverSlider;
+
+    using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
+    std::unique_ptr<Attachment> inputGainSliderAttachment,
+                                outputGainSliderAttachment,
+                                lowCrossoverSliderAttachment,
+                                midCrossoverSliderAttachment,
+                                highCrossoverSliderAttachment;
+};
+
 /**
 */
 class Projekt_zespoowy_2022AudioProcessorEditor  : public juce::AudioProcessorEditor
@@ -38,7 +76,8 @@ private:
     // access the processor object that created it.
     Projekt_zespoowy_2022AudioProcessor& audioProcessor;
 
-    Placeholder globalControls, bandLowControls, bandLowMidControls, bandHighMidControls, bandHighControls;
+    Placeholder /*globalControls,*/ bandLowControls, bandLowMidControls, bandHighMidControls, bandHighControls;
+    GlobalControls globalControls {audioProcessor.apvts};
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Projekt_zespoowy_2022AudioProcessorEditor)
 };
