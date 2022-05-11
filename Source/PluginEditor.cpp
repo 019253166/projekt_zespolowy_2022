ca70eee6a1c3523b896ace33ec229e87ec5b30a3
Jan Kucharczyk <kucharjan2@gmail.com>
2022-05-11 22:14:07 +0200

    etykiety parametrów + wygląd gałek

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,7 +9,242 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-int windowWidth = 1000;
+int windowWidth = 1000; 
+int windowHeight = 600;
+
+template<typename T>
+bool truncateKiloValue(T& value)
+{
+    if (value > static_cast<T>(999))
+    {
+        value /= static_cast<T>(1000);
+        return true;
+    }
+    return false;
+}
+
+juce::String getValString(const juce::RangedAudioParameter& param, bool getLow, juce::String suffix)
+{
+    juce::String str;
+
+    auto val = getLow ? param.getNormalisableRange().start :
+                        param.getNormalisableRange().end;
+    bool useK = truncateKiloValue(val);
+    str << val;
+    if (useK) str << "k";
+    str << suffix;
+    return str;
+}
+
+void LookAndFeel::drawRotarySlider(juce::Graphics& g,
+    int x,
+    int y,
+    int width,
+    int height,
+    float sliderPosProportional,
+    float rotaryStartAngle,
+    float rotaryEndAngle,
+    juce::Slider& slider)
+{
+    using namespace juce;
+
+    auto bounds = Rectangle<float>(x, y, width, height);
+
+    auto enabled = slider.isEnabled();
+
+    g.setColour(enabled ? Colours::black : Colours::darkgrey);
+    g.fillEllipse(bounds);
+
+    g.setColour(enabled ? Colours::darkcyan : Colours::grey);
+    g.drawEllipse(bounds, 3.f);
+
+    if (auto* rswl = dynamic_cast<RotarySliderWithLabels*>(&slider))
+    {
+        auto center = bounds.getCentre();
+        Path p;
+
+        Rectangle<float> r;
+        r.setLeft(center.getX() - 2);
+        r.setRight(center.getX() + 2);
+        r.setTop(bounds.getY());
+        r.setBottom(center.getY() - rswl->getTextHeight() * 1.5);
+
+        p.addRoundedRectangle(r, 2.f);
+
+        jassert(rotaryStartAngle < rotaryEndAngle);
+
+        auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
+
+        p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+
+        g.fillPath(p);
+
+        g.setFont(rswl->getTextHeight());
+        auto text = rswl->getDisplayString();
+        auto strWidth = g.getCurrentFont().getStringWidth(text);
+
+        r.setSize(strWidth + 4, rswl->getTextHeight() + 2);
+        r.setCentre(bounds.getCentre());
+
+        g.setColour(enabled ? Colours::black : Colours::darkgrey);
+        g.fillRect(r);
+
+        g.setColour(enabled ? Colours::white : Colours::lightgrey);
+        g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
+    }
+}
+
+void LookAndFeel::drawToggleButton(juce::Graphics& g,
+    juce::ToggleButton& toggleButton,
+    bool shouldDrawButtonAsHighlighted,
+    bool shouldDrawButtonAsDown)
+{
+    using namespace juce;
+
+    if (auto* pb = dynamic_cast<PowerButton*>(&toggleButton))
+    {
+        Path powerButton;
+
+        auto bounds = toggleButton.getLocalBounds();
+
+        auto size = jmin(bounds.getWidth(), bounds.getHeight()) - 6;
+        auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
+
+        float ang = 30.f; //30.f;
+
+        size -= 6;
+
+        powerButton.addCentredArc(r.getCentreX(),
+            r.getCentreY(),
+            size * 0.5,
+            size * 0.5,
+            0.f,
+            degreesToRadians(ang),
+            degreesToRadians(360.f - ang),
+            true);
+
+        powerButton.startNewSubPath(r.getCentreX(), r.getY());
+        powerButton.lineTo(r.getCentre());
+
+        PathStrokeType pst(2.f, PathStrokeType::JointStyle::curved);
+
+        auto color = toggleButton.getToggleState() ? Colours::dimgrey : Colour(0u, 172u, 1u);
+
+        g.setColour(color);
+        g.strokePath(powerButton, pst);
+        g.drawEllipse(r, 2);
+    }
+    else if (auto* analyzerButton = dynamic_cast<AnalyzerButton*>(&toggleButton))
+    {
+        auto color = !toggleButton.getToggleState() ? Colours::dimgrey : Colour(0u, 172u, 1u);
+
+        g.setColour(color);
+
+        auto bounds = toggleButton.getLocalBounds();
+        g.drawRect(bounds);
+
+        g.strokePath(analyzerButton->randomPath, PathStrokeType(1.f));
+    }
+}
+//==============================================================================
+void RotarySliderWithLabels::paint(juce::Graphics& g)
+{
+    using namespace juce;
+
+    auto startAng = degreesToRadians(180.f + 45.f);
+    auto endAng = degreesToRadians(180.f - 45.f) + MathConstants<float>::twoPi;
+
+    auto range = getRange();
+
+    auto sliderBounds = getSliderBounds();
+    auto bounds = getLocalBounds();
+
+    getLookAndFeel().drawRotarySlider(g,
+        sliderBounds.getX(),
+        sliderBounds.getY(),
+        sliderBounds.getWidth(),
+        sliderBounds.getHeight(),
+        static_cast<float>(jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0)),
+        startAng,
+        endAng,
+        *this);
+
+    auto center = sliderBounds.toFloat().getCentre();
+    auto radius = sliderBounds.getWidth() * 0.5f;
+
+    g.setColour(Colours::white);
+    g.setFont(getTextHeight());
+
+    auto numChoices = labels.size();
+    for (int i = 0; i < numChoices; ++i)
+    {
+        auto pos = labels[i].pos;
+        jassert(0.f <= pos);
+        jassert(pos <= 1.f);
+
+        auto ang = jmap(pos, 0.f, 1.f, startAng, endAng);
+
+        auto c = center.getPointOnCircumference(radius + getTextHeight() * 0.5f + 1, ang);
+
+        Rectangle<float> r;
+        auto str = labels[i].label;
+        r.setSize(g.getCurrentFont().getStringWidth(str), getTextHeight());
+        r.setCentre(c);
+        r.setY(r.getY() + getTextHeight());
+
+        g.drawFittedText(str, r.toNearestInt(), juce::Justification::centred, 1);
+    }
+
+}
+
+juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
+{
+    auto bounds = getLocalBounds();
+
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+
+    size -= getTextHeight() * 2;
+    juce::Rectangle<int> r;
+    r.setSize(size, size);
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2);
+
+    return r;
+
+}
+
+juce::String RotarySliderWithLabels::getDisplayString() const
+{
+    if (auto* choiceParam = dynamic_cast<juce::AudioParameterChoice*>(param))
+        return choiceParam->getCurrentChoiceName();
+
+    juce::String str;
+    bool addK = false;
+
+    if (auto* floatParam = dynamic_cast<juce::AudioParameterFloat*>(param))
+    {
+        float val = getValue();
+
+        addK = truncateKiloValue(val);
+
+        str = juce::String(val, (addK ? 2 : 0));
+    }
+    else
+    {
+        jassertfalse; //this shouldn't happen!
+    }
+
+    if (suffix.isNotEmpty())
+    {
+        str << " ";
+        if (addK)
+            str << "k";
+
+        str << suffix;
+    }
+
+    return str;
+}
 
 Placeholder::Placeholder()
 {
@@ -24,22 +259,39 @@ GlobalControls::GlobalControls(juce::AudioProcessorValueTreeState& apvts)
     using namespace Parameters;
     const auto& parameters = GetParameters();
 
+    auto getParamHelper = [&parameters, &apvts](const auto& name) -> auto&
+    {
+        return getParam(apvts, parameters, name);
+    };
+
+    inputGainSlider = std::make_unique<RSWL>(getParamHelper(Names::Input_Gain), "dB");
+    outputGainSlider = std::make_unique<RSWL>(getParamHelper(Names::Output_Gain), "dB");
+    lowCrossoverSlider = std::make_unique<RSWL>(getParamHelper(Names::Low_LowMid_Crossover_Freq), "Hz");
+    midCrossoverSlider = std::make_unique<RSWL>(getParamHelper(Names::LowMid_HighMid_Crossover_Freq), "Hz");
+    highCrossoverSlider = std::make_unique<RSWL>(getParamHelper(Names::HighMid_High_Crossover_Freq), "Hz");
+
     auto makeAttachmentHelper = [&parameters, &apvts](auto& attachment, const auto& name, auto& slider)
     {
         makeAttachment(attachment, apvts, parameters, name, slider);
     };
 
-    makeAttachmentHelper(inputGainSliderAttachment, Names::Input_Gain, inputGainSlider);
-    makeAttachmentHelper(outputGainSliderAttachment, Names::Output_Gain, outputGainSlider);
-    makeAttachmentHelper(lowCrossoverSliderAttachment, Names::Low_LowMid_Crossover_Freq, lowCrossoverSlider);
-    makeAttachmentHelper(midCrossoverSliderAttachment, Names::LowMid_HighMid_Crossover_Freq, midCrossoverSlider);
-    makeAttachmentHelper(highCrossoverSliderAttachment, Names::HighMid_High_Crossover_Freq, highCrossoverSlider);
+    makeAttachmentHelper(inputGainSliderAttachment, Names::Input_Gain, *inputGainSlider);
+    makeAttachmentHelper(outputGainSliderAttachment, Names::Output_Gain, *outputGainSlider);
+    makeAttachmentHelper(lowCrossoverSliderAttachment, Names::Low_LowMid_Crossover_Freq, *lowCrossoverSlider);
+    makeAttachmentHelper(midCrossoverSliderAttachment, Names::LowMid_HighMid_Crossover_Freq, *midCrossoverSlider);
+    makeAttachmentHelper(highCrossoverSliderAttachment, Names::HighMid_High_Crossover_Freq, *highCrossoverSlider);
 
-    addAndMakeVisible(inputGainSlider);
-    addAndMakeVisible(outputGainSlider);
-    addAndMakeVisible(lowCrossoverSlider);
-    addAndMakeVisible(midCrossoverSlider);
-    addAndMakeVisible(highCrossoverSlider);
+    addLabelPairs(inputGainSlider->labels, getParamHelper(Names::Input_Gain), "dB");
+    addLabelPairs(outputGainSlider->labels, getParamHelper(Names::Output_Gain), "dB");
+    addLabelPairs(lowCrossoverSlider->labels, getParamHelper(Names::Low_LowMid_Crossover_Freq), "Hz");
+    addLabelPairs(midCrossoverSlider->labels, getParamHelper(Names::LowMid_HighMid_Crossover_Freq), "Hz");
+    addLabelPairs(highCrossoverSlider->labels, getParamHelper(Names::HighMid_High_Crossover_Freq), "Hz");
+
+    addAndMakeVisible(*inputGainSlider);
+    addAndMakeVisible(*outputGainSlider);
+    addAndMakeVisible(*lowCrossoverSlider);
+    addAndMakeVisible(*midCrossoverSlider);
+    addAndMakeVisible(*highCrossoverSlider);
 }
 
 void GlobalControls::paint(juce::Graphics& g)
@@ -49,25 +301,34 @@ void GlobalControls::paint(juce::Graphics& g)
     g.setColour(Colours::darkgrey);
     g.fillAll();
 
-    bounds.reduce(3, 3);
+    bounds.reduce(1, 3);
     g.setColour(Colours::black);
     g.fillRoundedRectangle(bounds.toFloat(), 3);
 }
 
 void GlobalControls::resized()
 {
-    auto bounds = getLocalBounds();
+    auto bounds = getLocalBounds().reduced(4);
     using namespace juce;
 
     FlexBox flexBox;
     flexBox.flexDirection = FlexBox::Direction::row;
     flexBox.flexWrap = FlexBox::Wrap::noWrap;
 
-    flexBox.items.add(FlexItem(inputGainSlider).withFlex(1.f));
-    flexBox.items.add(FlexItem(lowCrossoverSlider).withFlex(1.f));
-    flexBox.items.add(FlexItem(midCrossoverSlider).withFlex(1.f));
-    flexBox.items.add(FlexItem(highCrossoverSlider).withFlex(1.f));
-    flexBox.items.add(FlexItem(outputGainSlider).withFlex(1.f));
+    auto spacer = FlexItem().withWidth(4);
+    auto endCap = FlexItem().withWidth(6);
+
+    flexBox.items.add(endCap);
+    flexBox.items.add(FlexItem(*inputGainSlider).withFlex(1.f));
+    flexBox.items.add(spacer);
+    flexBox.items.add(FlexItem(*lowCrossoverSlider).withFlex(1.f));
+    flexBox.items.add(spacer);
+    flexBox.items.add(FlexItem(*midCrossoverSlider).withFlex(1.f));
+    flexBox.items.add(spacer);
+    flexBox.items.add(FlexItem(*highCrossoverSlider).withFlex(1.f));
+    flexBox.items.add(spacer);
+    flexBox.items.add(FlexItem(*outputGainSlider).withFlex(1.f));
+    flexBox.items.add(endCap);
 
     flexBox.performLayout(bounds);
 }
@@ -83,7 +344,7 @@ Projekt_zespoowy_2022AudioProcessorEditor::Projekt_zespoowy_2022AudioProcessorEd
     addAndMakeVisible(bandLowMidControls);
     addAndMakeVisible(bandHighMidControls);
     addAndMakeVisible(bandHighControls);
-    setSize (windowWidth, 600);
+    setSize (windowWidth, windowHeight);
 }
 
 Projekt_zespoowy_2022AudioProcessorEditor::~Projekt_zespoowy_2022AudioProcessorEditor()
@@ -106,7 +367,7 @@ void Projekt_zespoowy_2022AudioProcessorEditor::resized()
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
     auto bounds = getLocalBounds();
-    globalControls.setBounds(bounds.removeFromTop(64));
+    globalControls.setBounds(bounds.removeFromTop(windowHeight / 6));
     bandLowControls.setBounds(bounds.removeFromLeft(windowWidth / 4));
     bandLowMidControls.setBounds(bounds.removeFromLeft(windowWidth / 4));
     bandHighMidControls.setBounds(bounds.removeFromLeft(windowWidth / 4));
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -11,6 +11,82 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct LookAndFeel : juce::LookAndFeel_V4
+{
+    void drawRotarySlider(juce::Graphics&,
+        int x, int y, int width, int height,
+        float sliderPosProportional,
+        float rotaryStartAngle,
+        float rotaryEndAngle,
+        juce::Slider&) override;
+
+    void drawToggleButton(juce::Graphics& g,
+        juce::ToggleButton& toggleButton,
+        bool shouldDrawButtonAsHighlighted,
+        bool shouldDrawButtonAsDown) override;
+};
+
+struct RotarySliderWithLabels : juce::Slider
+{
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+        juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+            juce::Slider::TextEntryBoxPosition::NoTextBox),
+        param(&rap),
+        suffix(unitSuffix)
+    {
+        setLookAndFeel(&lnf);
+    }
+
+    ~RotarySliderWithLabels()
+    {
+        setLookAndFeel(nullptr);
+    }
+
+    struct LabelPos
+    {
+        float pos;
+        juce::String label;
+    };
+
+    juce::Array<LabelPos> labels;
+
+    void paint(juce::Graphics& g) override;
+    juce::Rectangle<int> getSliderBounds() const;
+    int getTextHeight() const { return 14; }
+    juce::String getDisplayString() const;
+private:
+    LookAndFeel lnf;
+
+    juce::RangedAudioParameter* param;
+    juce::String suffix;
+};
+
+struct PowerButton : juce::ToggleButton { };
+
+struct AnalyzerButton : juce::ToggleButton
+{
+    void resized() override
+    {
+        auto bounds = getLocalBounds();
+        auto insetRect = bounds.reduced(4);
+
+        randomPath.clear();
+
+        juce::Random r;
+
+        randomPath.startNewSubPath(insetRect.getX(),
+            insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+
+        for (auto x = insetRect.getX() + 1; x < insetRect.getRight(); x += 2)
+        {
+            randomPath.lineTo(x,
+                insetRect.getY() + insetRect.getHeight() * r.nextFloat());
+        }
+    }
+
+    juce::Path randomPath;
+};
+
 //==============================================================================
 struct Placeholder : juce::Component
 {
@@ -41,6 +117,32 @@ void makeAttachment(std::unique_ptr<Attachment>& attachment, APVTS& apvts, const
     attachment = std::make_unique<Attachment>(apvts, parameters.at(name), slider);
 }
 
+template<
+    typename APVTS,
+    typename Params,
+    typename Name>
+
+juce::RangedAudioParameter& getParam(APVTS& apvts, const Params& params, const Name& name)
+{
+    auto param = apvts.getParameter(params.at(name));
+    jassert(param != nullptr);
+    return *param;
+}
+
+juce::String getValString(const juce::RangedAudioParameter& param, bool getLow, juce::String suffix);
+
+template<
+    typename Labels,
+    typename ParamType,
+    typename SuffixType>
+
+void addLabelPairs(Labels& labels, const ParamType& param, const SuffixType& suffix)
+{
+    labels.clear();
+    labels.add({ 0.f, getValString(param, true, suffix) });
+    labels.add({ 1.f, getValString(param, false, suffix) });
+}
+
 struct GlobalControls : juce::Component
 {
     GlobalControls(juce::AudioProcessorValueTreeState& apvts);
@@ -49,7 +151,8 @@ struct GlobalControls : juce::Component
 
     void resized() override;
 private:
-    RotarySlider inputGainSlider, outputGainSlider, lowCrossoverSlider, midCrossoverSlider, highCrossoverSlider;
+    using RSWL = RotarySliderWithLabels;
+    std::unique_ptr<RSWL> inputGainSlider, outputGainSlider, lowCrossoverSlider, midCrossoverSlider, highCrossoverSlider;
 
     using Attachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     std::unique_ptr<Attachment> inputGainSliderAttachment,
